5f756085ee02e64ee00c33717e69a0ed0e265c73
Chris Chabot <chris.chabot@memgraph.io>
2024-12-02 17:03:00 +0000

    Env vars are now being picked up correctly by the shell

--- Dockerfile
+++ Dockerfile
@@ -126,7 +126,7 @@ RUN echo '/home/node/.pm2/logs/*.log /home/node/app/cli/build/logs/*.log {\n\
     endscript\n\
 }"' > /etc/logrotate.d/pm2
 
-RUN echo -e "\n. /home/node/config/env\n" >> /etc/profile
+RUN echo -e "\nsource /home/node/config/env\n" >> /etc/profile
 
 USER node
 WORKDIR /home/node/app
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+## Pick up existing environment variables if already set
+if [ -f /home/node/config/env ]; then
+    . /home/node/config/env
+fi
 
 ## Network specific settings, change these for every network relaunch
 
 APP_MONITOR=${APP_MONITOR:-"54.185.250.216"}
 RPC_SERVER_URL=${RPC_SERVER_URL:-"https://atomium.shardeum.org"}
-EXISTING_ARCHIVERS=${EXISTING_ARCHIVERS:-'[{"ip":"34.68.218.222","port":4000,"publicKey":"64a3833499130406550729ab20f6bec351d04ec9be3e5f0144d54f01d4d18c45"},{"ip":"34.174.86.241","port":4000,"publicKey":"9b4ba46439ea6cafc6b20d971ab0ef0f21b415c27482652efac96fd61a76d73c"},{"ip":"34.48.51.73","port":4000,"publicKey":"ea72ef63e27cb960bfe02f17d40e74b5c28437af1d0df83dd21ba2084596789f"}]'}
+EXISTING_ARCHIVERS=${EXISTING_ARCHIVERS:-"[{\"ip\":\"34.68.218.222\",\"port\":4000,\"publicKey\":\"64a3833499130406550729ab20f6bec351d04ec9be3e5f0144d54f01d4d18c45\"},{\"ip\":\"34.174.86.241\",\"port\":4000,\"publicKey\":\"9b4ba46439ea6cafc6b20d971ab0ef0f21b415c27482652efac96fd61a76d73c\"},{\"ip\":\"34.48.51.73\",\"port\":4000,\"publicKey\":\"ea72ef63e27cb960bfe02f17d40e74b5c28437af1d0df83dd21ba2084596789f\"}]"}
 NEXT_PUBLIC_RPC_URL=${NEXT_PUBLIC_RPC_URL:-"https://atomium.shardeum.org"}
 NEXT_EXPLORER_URL=${NEXT_EXPLORER_URL:-"https://explorer-atomium.shardeum.org"}
 
@@ -70,6 +74,8 @@ if [ -z "$LOCALLANIP" ]; then
     LOCALLANIP=$(get_ip)
 fi
 
+
+## If the env file does not exist, create it so they're loaded automatically next time the container is started
 if [ ! -f /home/node/config/env ]; then
     cat >/home/node/config/env <<EOL
 APP_MONITOR=$APP_MONITOR
@@ -88,11 +94,8 @@ export APP_MONITOR RPC_SERVER_URL EXISTING_ARCHIVERS NEXT_PUBLIC_RPC_URL NEXT_EX
 EOL
 fi
 
-## Make variables available to the CLI app which uses them to start the validator
-export APP_MONITOR RPC_SERVER_URL EXISTING_ARCHIVERS NEXT_PUBLIC_RPC_URL NEXT_EXPLORER_URL INT_IP SHMEXT SHMINT DASHPORT RUNDASHBOARD EXT_IP SERVERIP
 
 ## Ensure the certificates for the GUI exist in the config directory
-
 
 if [ ! -f "/home/node/config/CA.cnf" ]; then
     cd /home/node/config
