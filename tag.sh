e201d58492fecb790e2ef7be2a8690e4ae2f4374
Chris Chabot <chris.chabot@memgraph.io>
2024-12-13 20:51:02 +0000

    Making life easier by automating tagging latest and creating manifests for arm64/amd64 builds

---
+++ tag.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TAG=$1
+
+if [ -z "$TAG" ]; then
+    echo "No tag provided. Exiting."
+    exit 1
+fi
+
+echo "Pulling images"
+
+docker pull ghcr.io/shardeum/shardeum-validator-arm64:$TAG
+docker pull ghcr.io/shardeum/shardeum-validator-amd64:$TAG
+
+echo "Tagging latest"
+
+docker tag ghcr.io/shardeum/shardeum-validator-arm64:$TAG ghcr.io/shardeum/shardeum-validator-arm64:latest
+docker tag ghcr.io/shardeum/shardeum-validator-amd64:$TAG ghcr.io/shardeum/shardeum-validator-amd64:latest
+
+echo "Pushing latest"
+
+docker push ghcr.io/shardeum/shardeum-validator-arm64:latest
+docker push ghcr.io/shardeum/shardeum-validator-amd64:latest
+
+echo "Creating $TAG manifest"
+
+docker manifest create ghcr.io/shardeum/shardeum-validator:$TAG ghcr.io/shardeum/shardeum-validator-arm64:$TAG ghcr.io/shardeum/shardeum-validator-amd64:$TAG
+docker manifest push ghcr.io/shardeum/shardeum-validator:$TAG
+
+echo "Creating :latest manifest"
+
+docker manifest create ghcr.io/shardeum/shardeum-validator:latest ghcr.io/shardeum/shardeum-validator-arm64:latest ghcr.io/shardeum/shardeum-validator-amd64:latest
+docker manifest push ghcr.io/shardeum/shardeum-validator:latest
+
+echo "Done"
