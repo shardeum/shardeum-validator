d4d31dd2230d803b5f405d851f493757856d9b93
Chris Chabot <chris.chabot@memgraph.io>
2024-12-18 09:46:34 +0000

    Check the entered password against the pw rules, and it it doesnt match prompt again with a hint on what to change

--- scripts/set-password.sh
+++ scripts/set-password.sh
@@ -14,7 +14,6 @@ docker-safe() {
   fi
 }
 
-
 read_password() {
   # Clear input buffer to avoid processing leftover inputs
   stty -icanon min 0 time 0  # Set non-canonical mode temporarily
@@ -23,23 +22,22 @@ read_password() {
 
   local CHARCOUNT=0
   local PASSWORD=""
-  while IFS= read -p "$PROMPT" -r -s -n 1 CHAR
-  do
+  while IFS= read -p "$PROMPT" -r -s -n 1 CHAR; do
     # Enter - accept password
-    if [[ $CHAR == $'\0' ]] ; then
+    if [[ $CHAR == $'\0' ]]; then
       break
     fi
     # Backspace
-    if [[ $CHAR == $'\177' ]] ; then
-      if [ $CHARCOUNT -gt 0 ] ; then
-        CHARCOUNT=$((CHARCOUNT-1))
+    if [[ $CHAR == $'\177' ]]; then
+      if [ $CHARCOUNT -gt 0 ]; then
+        CHARCOUNT=$((CHARCOUNT - 1))
         PROMPT=$'\b \b'
         PASSWORD="${PASSWORD%?}"
       else
         PROMPT=''
       fi
     else
-      CHARCOUNT=$((CHARCOUNT+1))
+      CHARCOUNT=$((CHARCOUNT + 1))
       PROMPT='*'
       PASSWORD+="$CHAR"
     fi
@@ -47,16 +45,60 @@ read_password() {
   echo $PASSWORD
 }
 
-echo  "Password requirements: "
-echo -n -e "min 8 characters, at least 1 lower case letter, at least 1 upper case letter, at least 1 number, at least 1 special character !@#$%^&*()_+$ \nSet the password to access the Dashboard:"
-DASHPASS=$(read_password)
+check_password() {
+  local PASSWORD=$1
+  local ERRORS=""
 
-if [ -z "$DASHPASS" ]; then
+  if [[ ${#PASSWORD} -lt 8 ]]; then
+    ERRORS+="Password must be at least 8 characters long.\n"
+  fi
+
+  if ! [[ "$PASSWORD" =~ [a-z] ]]; then
+    ERRORS+="Password must contain at least one lowercase letter.\n"
+  fi
+
+  if ! [[ "$PASSWORD" =~ [A-Z] ]]; then
+    ERRORS+="Password must contain at least one uppercase letter.\n"
+  fi
+
+  if ! [[ "$PASSWORD" =~ [0-9] ]]; then
+    ERRORS+="Password must contain at least one number.\n"
+  fi
+
+  # Corrected special character check:
+  if ! [[ "$PASSWORD" =~ [!@#$%^\&*()_+] ]]; then
+    ERRORS+="Password must contain at least one special character (!@#$%^&*()_+$).\n"
+  fi
+
+  if [[ -n "$ERRORS" ]]; then
+    echo -e "\nInvalid password. Please correct the following:\n$ERRORS"
+    return 1
+  else
+    return 0
+  fi
+}
+
+echo "Password requirements: "
+echo -e "min 8 characters, at least 1 lower case letter, at least 1 upper case letter, at least 1 number, at least 1 special character !@#$%^&*()_+$ \n"
+
+while true; do
+  echo -n "Enter the password for accessing the Dashboard: "
+  DASHPASS=$(read_password)
+
+  if [ -z "$DASHPASS" ]; then
     echo "Password cannot be empty"
-    exit 1
-fi
+    continue
+  fi
+  echo
+  if check_password "$DASHPASS"; then
+    break
+  fi
+done
 
-docker-safe exec -it shardeum-validator operator-cli gui set password "$DASHPASS" 
+# Escape $ characters for docker command
+ESCAPED_DASHPASS=$(printf '%s' "$DASHPASS" | sed 's/\$/\\\$/g')
 
-echo 
+docker-safe exec -it shardeum-validator operator-cli gui set password "$ESCAPED_DASHPASS"
+
+echo
 echo
