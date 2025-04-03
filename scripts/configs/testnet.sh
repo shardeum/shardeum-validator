71f2b440c7ecbf66e9873a7f495121133674a0d9
Aniket Divekar <aniket.divekar@gmail.com>
2025-04-03 11:35:32 +0530

    Used DNS for RPC and Explorer in testnet

--- scripts/configs/testnet.sh
+++ scripts/configs/testnet.sh
@@ -11,10 +11,10 @@ export NEXT_PUBLIC_CHAIN_ID=$CHAIN_ID
 
 # External Service Configuration
 export APP_MONITOR="34.56.47.170"
-export RPC_SERVER_URL="http://34.60.100.120:8000"
+export RPC_SERVER_URL="https://api-testnet.shardeum.org"
 export EXISTING_ARCHIVERS='[{"ip":"104.197.117.164","port":4000,"publicKey":"d831bb7c09db45d47338af23ab50cac5d29ef8f3a2cd274dd741370aa472d6c1"},{"ip":"34.139.3.222","port":4000,"publicKey":"1c42a7f9cca36e13e590ae00c1124c5a1f696c879da210ffcdccb312d08c8214"},{"ip":"35.233.192.167","port":4000,"publicKey":"d1721c924394ae1ff3e9ea22af15962e045511fde05ed0b56f0a8c36eb161d75"}]'
-export NEXT_PUBLIC_RPC_URL="http://34.60.100.120:8000"
-export NEXT_PUBLIC_EXPLORER_URL="http://34.136.118.2:6001"
+export NEXT_PUBLIC_RPC_URL="https://api-testnet.shardeum.org"
+export NEXT_PUBLIC_EXPLORER_URL="https://explorer-testnet.shardeum.org"
 
 # Network parameters
 export minNodes=256
