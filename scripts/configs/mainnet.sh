43f22571309eacb5ed6e53a5bba949c615636fa1
Shane <jonas.shane@gmail.com>
2025-04-08 14:54:40 -0400

    fix: mainnet configs for testing (#17)

--- scripts/configs/mainnet.sh
+++ scripts/configs/mainnet.sh
@@ -10,15 +10,15 @@ export CHAIN_ID="8118"
 export NEXT_PUBLIC_CHAIN_ID=$CHAIN_ID
 
 ## External Service Configuration
-export APP_MONITOR="0.0.0.0"
-export RPC_SERVER_URL="http://0.0.0.0:8000"
-export EXISTING_ARCHIVERS='[{"ip":"0.0.0.0","port":4000,"publicKey":""},{"ip":"0.0.0.0","port":4000,"publicKey":""},{"ip":"0.0.0.0","port":4000,"publicKey":""}]'
-export NEXT_PUBLIC_RPC_URL="http://0.0.0.0:8000"
-export NEXT_PUBLIC_EXPLORER_URL="http://0.0.0.0:6001"
+export APP_MONITOR="104.154.117.74"
+export RPC_SERVER_URL="http://35.238.62.173:8000"
+export EXISTING_ARCHIVERS='[{"ip":"35.238.248.68","port":4000,"publicKey":"e25aa3465e43ecdee2cceddc381d137a4fa4c174144144683e0ea2a42a3801ff"},{"ip":"34.23.94.188","port":4000,"publicKey":"4a6c6ddc9cedb2f4b0ae90a9fd36e705abf572ba971585b429329408bfdfb20e"},{"ip":"34.145.9.44","port":4000,"publicKey":"5278029906ac9b0ae79b96364ef3c7aaa09abd4a36397f0bd5ca7faedd4c575c"}]'
+export NEXT_PUBLIC_RPC_URL="http://35.238.62.173:8000"
+export NEXT_PUBLIC_EXPLORER_URL="http://34.72.93.203:6001"
 
 # Network parameters
-export minNodes=256
-export baselineNodes=256
+export minNodes=360
+export baselineNodes=360
 export nodesPerConsensusGroup=128
 export maxNodes=1280
 export enableProblematicNodeRemoval=true
