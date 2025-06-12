e1f28934741dbcb69e3d38bf09deaf37f3163a96
Justin Lauria <justin@shardeum.org>
2025-06-12 15:40:13 -0500

    Adding DNS for RPC urls

--- scripts/configs/devnet-us.sh
+++ scripts/configs/devnet-us.sh
@@ -11,9 +11,9 @@ export NEXT_PUBLIC_CHAIN_ID=$CHAIN_ID
 
 # External Service Configuration
 export APP_MONITOR="34.75.10.66"
-export RPC_SERVER_URL="http://34.148.186.123:8000"
+export RPC_SERVER_URL="https://devnetus.shardeum.org"
 export EXISTING_ARCHIVERS='[{"ip":"35.229.75.252","port":4000,"publicKey":"2db7c949632d26b87d7e7a5a4ad41c306f63ee972655121a37c5e4f52b00a542"}]'
-export NEXT_PUBLIC_RPC_URL="http://34.148.186.123:8000"
+export NEXT_PUBLIC_RPC_URL="https://devnetus.shardeum.org"
 export NEXT_PUBLIC_EXPLORER_URL="https://explorer-devnetus.shardeum.org"
 
 export LOAD_JSON_CONFIGS=/usr/src/app/environments/devnet.config.json
