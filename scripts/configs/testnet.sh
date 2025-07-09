0a3052b01515bc101de0e3662fae1309f6faaf2b
Aniket Divekar <aniket.divekar@gmail.com>
2025-07-09 13:16:07 +0530

    Updated archiver and rpc IPs

--- scripts/configs/testnet.sh
+++ scripts/configs/testnet.sh
@@ -12,8 +12,8 @@ export NEXT_PUBLIC_CHAIN_ID=$CHAIN_ID
 # External Service Configuration
 export APP_MONITOR="34.136.247.113"
 export RPC_SERVER_URL="https://api-testnet.shardeum.org"
-export EXISTING_ARCHIVERS='[{"ip":"34.134.161.138","port":4000,"publicKey":"d831bb7c09db45d47338af23ab50cac5d29ef8f3a2cd274dd741370aa472d6c1"}]'
-export NEXT_PUBLIC_RPC_URL="https://api-testnet.shardeum.org"
+export EXISTING_ARCHIVERS='[{"ip":"34.172.76.241","port":4000,"publicKey":"d831bb7c09db45d47338af23ab50cac5d29ef8f3a2cd274dd741370aa472d6c1"}]'
+export NEXT_PUBLIC_RPC_URL="http://44.210.15.136:8090"
 export NEXT_PUBLIC_EXPLORER_URL="https://explorer-testnet.shardeum.org"
 
 export LOAD_JSON_CONFIGS=/usr/src/app/environments/testnet.config.json
